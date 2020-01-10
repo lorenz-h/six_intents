3e6d2a519e823911ac99712aa500f16048caf6c4
lorenz-h <lorenz.hetzel@me.com>
2020-01-10 11:50:09 +0900

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
-from utils import module_path
+from six_intents.utils import module_path
 
 if __name__ == "__main__":
     module_root_dir = module_path("")
-    subprocess.call(["git", "clone", "https://github.com/facebookresearch/InferSent.git"], cwd=module_root_dir)
+    subprocess.call(["git", "submodule", "init", "--recursive"], cwd=module_root_dir)
     
